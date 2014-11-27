0b7c402cf3086d7a9727b7dae690eef3dabe089c
Scott Harrison <scott.harrison25@gmail.com>
2014-11-27 00:33:39 -0600

    Rubocop changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,10 @@
 class CategoriesController < ApplicationController
-  
   def index
     @categories = Category.all
   end
-  
+
   def show
     @products = Category.find_by_id(params[:id]).products
     @categories = Category.all
   end
-  
 end
