54cb86f8b78a79c6327f61fa66da228fe2afdc4a
Scott Harrison <scott.harrison25@gmail.com>
2014-11-27 13:27:42 -0600

    50% milestone and bootstrap configuration start

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   
   root to: 'main#index'
+  
+  get 'contact' => 'contact#index', as: 'contact'
+  get 'about' => 'about#index', as: 'about'
 
   get 'products' => 'products#index', as: 'products'
   get 'products/:id' => 'products#show', as: 'product'
