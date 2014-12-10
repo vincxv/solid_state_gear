3ac5636d62318a5059b49ab1b32a51db7d3c96dd
Scott Harrison <scott.harrison25@gmail.com>
2014-12-09 19:04:30 -0600

    Completed editable about and contact pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   def about
-    @page = Page.where(permalink: about)
+    @page = Page.where(permalink: 'about').take
+    @categories = Category.all
   end
 
   def contact
-    @page = Page.where(permalink: contact)
+    @page = Page.where(permalink: 'contact').take
+    @categories = Category.all
   end
 end
