f00dd4d4fc0c54c0a2e877a3c75fd9be34d5ebf7
Scott Harrison <scott.harrison25@gmail.com>
2014-12-11 15:01:10 -0600

    Rubocop corrections

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,14 +3,17 @@ class MainController < ApplicationController
     @products = Product.all.page(params[:page]).per(6)
     @categories = Category.all
   end
-  
+
   def featured
-    @products = Product.where(featured: true).page(params[:page]).per(6)
+    @products = Product.where(featured: true)
+                .page(params[:page]).per(6)
     @categories = Category.all
   end
-  
+
   def new
-    @products = Product.where('created_at > ?', 3.days.ago ).order(:updated_at).page(params[:page]).per(6)
+    @products = Product.where('created_at > ?', 3.days.ago)
+                .order(:updated_at)
+                .page(params[:page]).per(6)
     @categories = Category.all
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,16 +19,16 @@ class ProductsController < ApplicationController
       @products = Product.where('name LIKE ?', keywords)
     end
   end
-  
+
   def new
     @categories = Category.all
   end
-  
+
   def add_item_cart
     (session[:cart] ||= []) << params[:id]
     redirect_to :back
   end
-  
+
   def item_cart
     @categories = Category.all
     @products = Product.all
